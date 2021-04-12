c78fa04aa89898f4bcfb5d54e9f35463e9302740
Farhad Esmaeili <esmaili.farhad67@gmail.com>
2021-04-12 08:14:49 +0430

    .

--- APIGateway/APIGateway/Controllers/ShahabController.cs
+++ APIGateway/APIGateway/Controllers/ShahabController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using Oracle.ManagedDataAccess.Client;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Web.Http;
 
@@ -26,7 +27,7 @@ namespace APIGateway.Controllers
                     cmd.CommandTimeout = 60000;
                     cmd.Connection = conn;
                     cmd.CommandText = string.Format(@"select * from shahab where cfcifno ={0} ", input.cif);
-                   /// cmd.CommandText = string.Format(@"select * from shahab");
+                    /// cmd.CommandText = string.Format(@"select * from shahab");
 
 
                     cmd.CommandType = CommandType.Text;
@@ -39,6 +40,54 @@ namespace APIGateway.Controllers
                     }
                 }
                 return Ok(paramOut);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpPost]
+        //شرایط برداشت 
+        public IHttpActionResult GetWithDrawalCondition(APIGateway.ViewModel.Shahab.ParamWithDrawalCondition param)
+        {
+            try
+            {
+                List<ViewModel.Shahab.WithDrawalCondition> lstResult = new List<ViewModel.Shahab.WithDrawalCondition>();
+                using (OracleConnection conn = new OracleConnection(APIGateway.Properties.Settings.Default.oradb))
+                {
+                    conn.Open();
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.CommandTimeout = 60000;
+                    cmd.Connection = conn;
+                    cmd.CommandText = string.Format(@"
+select * from WITHDRAWAL_CONDITION t
+where t.BRANCH_CODE={0} and t.DEPOSIT_TYPE={1} and t.CUSTOMER_NO={2} and t.DEPOSIT_SERIAL={3}
+order by t.CONDITION_ID,t.PLACE_ID", param.BRANCH_CODE, param.DEPOSIT_TYPE, param.CUSTOMER_NO, param.DEPOSIT_SERIAL);
+
+                    cmd.CommandType = CommandType.Text;
+                    OracleDataReader dr = cmd.ExecuteReader();
+
+
+
+                    while (dr.Read())
+                    {
+                        ViewModel.Shahab.WithDrawalCondition withDrawalCondition = new ViewModel.Shahab.WithDrawalCondition()
+                        {
+                            ShareCustomerNo = dr["SHARE_CUSTOMER_NO"].ToString(),
+                            PlaceId = dr["PLACE_ID"].ToString(),
+                            ConditionId = dr["CONDITION_ID"].ToString(),
+                            BRANCH_CODE = dr["BRANCH_CODE"].ToString(),
+                            CUSTOMER_NO  = dr["CUSTOMER_NO"].ToString(),
+                            DEPOSIT_SERIAL = dr["DEPOSIT_SERIAL"].ToString(),
+                            DEPOSIT_TYPE = dr["DEPOSIT_TYPE"].ToString(),
+                        };
+                        lstResult.Add(withDrawalCondition);
+                    }
+                }
+                return Ok(lstResult);
             }
             catch (Exception ex)
             {
---
+++ APIGateway/APIGateway/ViewModel/Shahab/WithDrawalCondition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIGateway.ViewModel.Shahab
+{
+    public class WithDrawalCondition
+    {
+        public string BRANCH_CODE { get; set; }
+        public string DEPOSIT_TYPE { get; set; }
+        public string CUSTOMER_NO { get; set; }
+        public string DEPOSIT_SERIAL { get; set; }
+        public string ConditionId { get; set; }
+        public string PlaceId { get; set; }
+        public string ShareCustomerNo { get; set; }
+    }
+    public class ParamWithDrawalCondition {
+        public string BRANCH_CODE { get; set; }
+        public string DEPOSIT_TYPE { get; set; }
+        public string CUSTOMER_NO { get; set; }
+        public string DEPOSIT_SERIAL { get; set; }
+    }
+}
